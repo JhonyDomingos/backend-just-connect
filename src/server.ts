860e362452b2a1f273d320dc1acc9926324449ba
hallanpf <hallanpf@gmail.com>
2024-10-21 23:06:53 -0300

    Feat: Configuring and Adding the Swagger Documentation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import { router } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "../swagger_output.json";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof Error) {
