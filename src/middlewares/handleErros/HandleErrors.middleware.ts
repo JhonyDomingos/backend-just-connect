416e20e71c00347a50048ea26d167fa3d24949a3
JhonyDomingos <jhonatandomingossilva@outlook.com.br>
2024-10-22 23:47:53 -0300

    feat: Adding Exeption Treatment

---
+++ src/middlewares/handleErros/HandleErrors.middleware.ts
@@ -0,0 +1,31 @@
+import type { NextFunction, Request, Response } from "express";
+import { AppError } from "../../Error/AppError.error";
+import { ZodError } from "zod";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+class HandleErrors {
+  public static execute = (
+    error: unknown,
+    _: Request,
+    response: Response,
+    __: NextFunction
+  ) => {
+    
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
+
+    if (error instanceof ZodError) {
+      return response
+        .status(400)
+        .json({ message: error.flatten().fieldErrors });
+    }
+    if (error instanceof JsonWebTokenError) {
+      return response.status(400).json({ message: error.message });
+    }
+
+    return response.status(500).json({ message: "Internal Server Error" });
+  };
+}
+
+export { HandleErrors };
