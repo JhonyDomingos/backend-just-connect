c53b844fc977080772dd3ddc7a8666bfb024094f
Ana <anaclaraqb@gmail.com>
2024-10-22 21:57:58 -0300

    docs: Adding instructions to edit swagger

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -9,8 +9,31 @@ router.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-router.use('/users', userPublicRoutes);
+router.use('/public/users', userPublicRoutes);
 router.use('/users', userPrivateRoutes);
-router.use('/auth', sessionPublicRoutes);
+router.use('/public/auth', sessionPublicRoutes);
 
 export { router };
+
+/**
+ * Public routes:
+ * 
+ * POST - /public/users/register - Register user
+ * 201 ok created ou 400 bad request
+ * 
+ * GET - /public/users - List all users
+ * 200 ok
+ * 
+ * GET - /public/users/:id - Get one user
+ * 200 ok ou 400 bad request
+ * 
+ * POST - /public/auth - Login
+ * 200 ok ou 400 bad request
+ * 
+ * Authenticated routes:
+ * PUT - /users/:id - Edit user
+ * 200 ok ou 400 bad request ou 404 not found ou 401 unauthorized
+ * 
+ * DELETE - /users/:id - Delete user
+ * 204 ok no content ou 400 bad request ou 404 not found ou 401 unauthorized
+ */
