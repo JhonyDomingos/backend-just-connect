ff303b9526f26ba5502d060a36e538c7fb535d4c
hallanpf <hallanpf@gmail.com>
2024-10-30 15:08:36 -0300

    Feat: Created the service route to find the all posts of the user by username

---
+++ src/services/post/FindAllPostUsernameService.ts
@@ -0,0 +1,29 @@
+import prismaClient from '../../prisma';
+import { ReturnPostData } from "../../interfaces/post/PostType";
+import { UsernameRequest } from "../../interfaces/user/usernameRequest";
+
+class FindAllPostUsernameService {
+  async findAll({ username }: UsernameRequest): Promise<ReturnPostData[]> {
+
+    const user = await prismaClient.user.findUnique({
+      where: {
+        username
+      }
+    })
+
+    if (!user) {
+      throw new Error('User not found');
+      return [];
+    }
+    
+    const posts = await prismaClient.post.findMany({
+      where: {
+        user_id: user.id
+      }
+    });
+
+    return posts;
+  }
+}
+
+export { FindAllPostUsernameService };
