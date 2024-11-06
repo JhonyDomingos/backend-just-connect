01ccc8332b456f430f2e7c6e71a4e97851ea3180
JhonyDomingos <jhonatandomingossilva@outlook.com.br>
2024-11-06 02:11:12 -0300

    Refacort: excuding the old code

--- src/middlewares/auth/Auth.middleware.ts
+++ src/middlewares/auth/Auth.middleware.ts
@@ -1,37 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
-import { Payload } from "../../interfaces/auth/Payload";
 import { AppError } from "../../Error/AppError.error";
-
-export function isAuthenticated(
-  request: Request,
-  _: Response,
-  next: NextFunction
-) {
-  const authToken = request.headers.authorization;
-
-  if (!authToken) {
-    throw new AppError("Token is missing", 401);
-  }
-
-  const [, token] = authToken.split(" ");
-  if (!token) {
-    throw new AppError("Token is missing", 401);
-  }
-
-  const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
-
-  // request.user_id = sub;
-
-  return next();
-}
 
 class AuthMiddleware {
   public static execute = (
     request: Request,
     response: Response,
     next: NextFunction
-  ) => {
+  ): void => {
     const { authorization } = request.headers;
 
     if (!authorization) {
