9fdba0e0291287e9a90a33328ce941c337a20359
JhonyDomingos <jhonatandomingossilva@outlook.com.br>
2024-11-07 20:56:45 -0300

    Refactor: Reafactor mailoption content

--- src/routes/public/reset.password.public.routes.ts
+++ src/routes/public/reset.password.public.routes.ts
@@ -1,20 +1,19 @@
-import { Router } from 'express';
-import { AuthService } from '../../services/auth/AuthService';
+import { Router } from "express";
+import { AuthService } from "../../services/auth/AuthService";
 
 const resetPasswordRoutes: Router = Router();
 const authService = new AuthService();
 
-resetPasswordRoutes.post('/reset-password', async (req, res, next) => {
-    const { newPassword } = req.body;
-    const { token } = req.query.token as { token: string };
+resetPasswordRoutes.post("/reset-password", async (req, res, next) => {
+  const { newPassword } = req.body;
+  const token = req.query.token as string;
 
-    try {
-        await authService.resetPassword(token, newPassword);
-        res.send('Password has been reset');
-    } catch (err) {
-        next(err);
-    }
+  try {
+    await authService.resetPassword(token, newPassword);
+    res.send("Password has been reset");
+  } catch (err) {
+    next(err);
+  }
 });
 
 export { resetPasswordRoutes };
-
