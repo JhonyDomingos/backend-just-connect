f64140251eaa415524af67baf87cc8ccbe691e23
JhonyDomingos <jhonatandomingossilva@outlook.com.br>
2024-11-15 15:04:55 -0300

    refactor:  add the query log on database

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
+const prismaClient = new PrismaClient({log: ['query']});
+
 
 export default prismaClient;
