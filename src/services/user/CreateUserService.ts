41fc88dd719488b560bd242794cf9509001730ee
JhonyDomingos <jhonatandomingossilva@outlook.com.br>
2024-11-17 14:12:22 -0300

    Refactor: Removing the validation on the user service

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -2,34 +2,9 @@ import prismaClient from "../../prisma";
 import { hash } from "bcryptjs";
 import { CreateUserData, NewUserData } from "../../interfaces/user/UserTypes";
 import { userRegisteredSchema } from "../../schemas/userSchemas";
-import { AppError } from "../../Error/AppError.error";
 
 class CreateUserService {
   async execute(data: CreateUserData): Promise<NewUserData> {
-    if (!data.username) {
-      throw new AppError("Username incorrect", 400);
-    }
-
-    if (!data.email) {
-      throw new AppError("Email incorrect", 400);
-    }
-
-    const usernameAlreadyExists = await prismaClient.user.findFirst({
-      where: { username: data.username },
-    });
-
-    if (usernameAlreadyExists) {
-      throw new AppError("Username already exists", 400);
-    }
-
-    const emailAlreadyRegistered = await prismaClient.user.findFirst({
-      where: { email: data.email },
-    });
-
-    if (emailAlreadyRegistered) {
-      throw new AppError("Email is already registered", 400);
-    }
-
     const passwordHash = await hash(data.password, 10);
 
     const user = await prismaClient.user.create({
