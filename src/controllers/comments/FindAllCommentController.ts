26874a3147700cd65a1c67d07368a4e14425ea2a
JhonyDomingos <jhonatandomingossilva@outlook.com.br>
2024-11-22 15:13:14 -0300

    refactor: refactoring comments delete and find

--- src/controllers/comments/FindAllCommentController.ts
+++ src/controllers/comments/FindAllCommentController.ts
@@ -3,10 +3,11 @@ import { FindAllCommentService } from '../../services/comments/FindAllCommentSer
 
 class FindAllCommentController {
   async handle(_: Request, res: Response): Promise<Response> {
-    const { sub } = res.locals.decodedToken;
-
+    
+   
     const findAllCommentService = new FindAllCommentService();
-    const comments = await findAllCommentService.findAll(sub);
+    const comments = await findAllCommentService.findAll();
+    console.log(comments);
     return res.status(200).json(comments);
   }
 }
